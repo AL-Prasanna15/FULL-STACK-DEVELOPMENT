c81235ea304e7a87881d6e6dc641816be7b67639
AL-Prasanna15 <avulajoshna5@gmail.com>
2026-03-13 14:44:56 +0530

    Add files via upload

---
+++ drivingLicense.js
@@ -0,0 +1,12 @@
+let age=19;
+let hasEducation=true;
+let passedtest=true;
+let violations=0;
+if(age>=18&&hasEducation&&passedtest&&violations===0){
+    console.log("Eligible for license");
+    console.log("please proceed");
+}
+else{
+    console.log("not eligible");
+    console.log("requirements are not correct.PLEASE CHECK YOUR DETAILS");
+}
